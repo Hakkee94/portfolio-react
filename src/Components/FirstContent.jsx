7051e106b92bcf365b7b940307ddda27de2417a3
Hakkee94 <agasyandavid94@gmail.com>
2022-08-25 12:25:32 +0300

    prefinal changes

--- src/Components/FirstContent.jsx
+++ src/Components/FirstContent.jsx
@@ -18,7 +18,8 @@ const FirstContent = () => {
                     <SkillItem skill='Кроссбраузерная, валидная, семантичная, адаптивная верстка' src={imgs.HTML}/>
                     <SkillItem skill='Уверенные знания JavaScript (базовые ES6)' src={imgs.js}/>
                     <SkillItem skill='БЭМ, Bootstrap 3, CSS3, LESS, SASS' src={imgs.CSS3}/>
-                    <SkillItem skill='React - базовый уровень' src={imgs.reactLogo}/>
+                    <SkillItem skill='React - уверенные знания' src={imgs.reactLogo}/>
+                    <SkillItem skill='Redux - базовый уровнь' src='https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png'/>
                     <SkillItem skill='Опыт вёрстки сайтов и шаблонов по Figma' src={imgs.figma}/>
                     <SkillItem skill='Опыт работы с git, Github, IDE - WebStorm' src={imgs.github}/>
                     <SkillItem skill='Английский язык Upper Intermediate(B2)' src={imgs.English}/>
