8c6b82efcbc24b8bab73752c6de4c19278cbc509
Hakkee94 <agasyandavid94@gmail.com>
2022-08-29 17:01:30 +0300

    hotel changes

--- src/Components/SemiDivs.jsx
+++ src/Components/SemiDivs.jsx
@@ -7,7 +7,7 @@ const SemiDivs = () => {
             <OtherProjects fullProject title='Интернет магазин' info='Приложение интернет-магазина, написанное с помощью HTML, CSS, JS с функциями добавления и удаления названий, цен, изображений товаров' link='https://github.com/Hakkee94/David-layout/tree/dev/Internet-shop'/>
             <OtherProjects title='Figma land' info='Проект, созданный по макету Figma' link='https://github.com/Hakkee94/David-layout/tree/dev/homework/Figma-land'/>
             <OtherProjects title='Ryan Warner' info='Проект, созданный по макету Figma' link='https://github.com/Hakkee94/David-layout/tree/dev/homework/Ryan%20Warner'/>
-            <OtherProjects title='Underwater hotel' info='Проект, созданный по макету Figma' link='https://github.com/Hakkee94/David-layout/tree/dev/homework/Veles%20Figma'/>
+            <OtherProjects title='Underwater hotel' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/underground-hotel/'/>
         </div>
     );
 }
