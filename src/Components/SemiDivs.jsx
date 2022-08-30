9d7ecaaf6bb42dccc4e292faafffb71fe5ebd16d
Hakkee94 <agasyandavid94@gmail.com>
2022-08-30 09:34:41 +0300

    Ryan Warner add

--- src/Components/SemiDivs.jsx
+++ src/Components/SemiDivs.jsx
@@ -6,8 +6,8 @@ const SemiDivs = () => {
         <div className="semi-divs">
             <OtherProjects fullProject title='Интернет магазин' info='Приложение интернет-магазина, написанное с помощью HTML, CSS, JS с функциями добавления и удаления названий, цен, изображений товаров' link='https://github.com/Hakkee94/David-layout/tree/dev/Internet-shop'/>
             <OtherProjects title='Figma land' info='Проект, созданный по макету Figma' link='https://github.com/Hakkee94/David-layout/tree/dev/homework/Figma-land'/>
-            <OtherProjects title='Ryan Warner' info='Проект, созданный по макету Figma' link='https://github.com/Hakkee94/David-layout/tree/dev/homework/Ryan%20Warner'/>
-            <OtherProjects title='Underwater hotel' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/underground-hotel/'/>
+            <OtherProjects title='Underwater hotel' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/Figma-project2/'/>
+            <OtherProjects title='Ryan Warner' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/Figma-project3/'/>
         </div>
     );
 }
