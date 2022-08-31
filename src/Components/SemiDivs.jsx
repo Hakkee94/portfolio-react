fdcc88a177a636715faa8d5edf7d3dc6bd8a6a28
Hakkee94 <agasyandavid94@gmail.com>
2022-08-31 15:13:18 +0300

    prefinal commit

--- src/Components/SemiDivs.jsx
+++ src/Components/SemiDivs.jsx
@@ -5,7 +5,7 @@ const SemiDivs = () => {
     return (
         <div className="semi-divs">
             <OtherProjects fullProject title='Интернет магазин' info='Приложение интернет-магазина, написанное с помощью HTML, CSS, JS с функциями добавления и удаления названий, цен, изображений товаров' link='https://github.com/Hakkee94/David-layout/tree/dev/Internet-shop'/>
-            <OtherProjects title='Figma land' info='Проект, созданный по макету Figma' link='https://github.com/Hakkee94/David-layout/tree/dev/homework/Figma-land'/>
+            <OtherProjects title='Figma land' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/Figma-project1/'/>
             <OtherProjects title='Underwater hotel' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/Figma-project2/'/>
             <OtherProjects title='Ryan Warner' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/Figma-project3/'/>
         </div>
