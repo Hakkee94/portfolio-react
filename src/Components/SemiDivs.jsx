ff91823c3325fcd881ce18be7920f10c933a3e96
Hakkee94 <agasyandavid94@gmail.com>
2022-09-01 12:24:34 +0300

    add actual links

--- src/Components/SemiDivs.jsx
+++ src/Components/SemiDivs.jsx
@@ -4,7 +4,7 @@ import OtherProjects from "./OtherProjects";
 const SemiDivs = () => {
     return (
         <div className="semi-divs">
-            <OtherProjects fullProject title='Интернет магазин' info='Приложение интернет-магазина, написанное с помощью HTML, CSS, JS с функциями добавления и удаления названий, цен, изображений товаров' link='https://github.com/Hakkee94/David-layout/tree/dev/Internet-shop'/>
+            <OtherProjects fullProject title='Интернет магазин' info='Приложение интернет-магазина, написанное с помощью HTML, CSS, JS с функциями добавления и удаления названий, цен, изображений товаров' link='https://hakkee94.github.io/Internet-shop-app/'/>
             <OtherProjects title='Figma land' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/Figma-project1/'/>
             <OtherProjects title='Underwater hotel' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/Figma-project2/'/>
             <OtherProjects title='Ryan Warner' info='Проект, созданный по макету Figma' link='https://hakkee94.github.io/Figma-project3/'/>
