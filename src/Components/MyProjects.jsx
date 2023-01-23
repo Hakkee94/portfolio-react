55ff64026a32b84719e90fbf5987b00d86c70248
d.agasyan <d.agasyan@getproduction.ru>
2023-01-23 15:07:43 +0400

    add app beer

--- src/Components/MyProjects.jsx
+++ src/Components/MyProjects.jsx
@@ -11,6 +11,7 @@ const MyProjects = () => {
 
             <div className='first-project'>
                 <OtherProjects fullProject title='Redux приложение' info='Интернет магазин ноутбуков, написанный на React+Redux, использовался redux-toolkit' link='https://hakkee94.github.io/redux-cart-shop/'/>
+                <OtherProjects fullProject title='Next js приложение поиска пива по названию' info='Приложение, написанное с помощью react, next js, typescript' link='https://beer-app-hakkee94.vercel.app/'/>
                 <OtherProjects fullProject title='Интернет магазин' info='Приложение интернет-магазина, написанное с помощью HTML, CSS, JS с функциями добавления и удаления названий, цен, изображений товаров' link='https://hakkee94.github.io/Internet-shop-app/'/>
                 <OtherProjects fullProject title='Сайт курса для беременных' info='Сайт, написанный с помощью HTML, CSS, JS' link='https://hakkee94.github.io/pregnancy-course/'/>
                 <OtherProjects fullProject title='Сайт курса для женщин после беременности' info='Сайт, написанный с помощью HTML, CSS, JS' link='https://hakkee94.github.io/afterpregnancy-course/'/>
